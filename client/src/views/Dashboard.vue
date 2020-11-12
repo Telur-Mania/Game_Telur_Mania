76d712ed3c92994dae67d43f0e89476db7182d2e
RafiP <rafi.panji.p@gmail.com>
2020-11-12 23:48:02 +0700

    Basic Logic

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="about">
+  <div class="container">
     <h1>This is an about page</h1>
     <div
       v-for="(user, i) in onlineUsers"
       :key="i"
     >
       <h5>{{user}}</h5>
+    </div>
+
+    <div class="row">
+      <div class="col-4">
+        {{count1}}
+        <button @click="button1" class="btn btn-primary" type="button" >dor 1</button>
+      </div>
+      <div class="col-4">
+        {{count2}}
+        <button @click="button2" class="btn btn-warning" type="button">dor 2</button>
+      </div>
+      <div class="col-4">
+        {{count3}}
+        <button @click="button3" class="btn btn-danger" type="button">dor 3</button>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +31,9 @@ export default {
   name: 'Dashboard',
   data () {
     return {
-      // onlineUsers: []
+      count1: 100,
+      count2: 100,
+      count3: 100
     }
   },
   computed: mapState([
@@ -24,10 +41,34 @@ export default {
     'onlineUsers'
   ]),
   sockets: {
-    // userLogin(onlineUsers) {
-    //   console.log(onlineUsers)
-    //   this.onlineUsers = onlineUsers
-    // }
+    count1(count1Left) {
+      this.count1 = count1Left
+      // console.log(count1Left)
+    }
+    ,
+    count2(count2Left) {
+      this.count2 = count2Left
+      // console.log(count1Left)
+    }
+     ,
+    count3(count3Left) {
+      this.count3 = count3Left
+      // console.log(count1Left)
+    }
+  },
+  methods: {
+    button1() {
+      this.count1--
+      this.$socket.emit('count1', {count1: this.count1})
+    },
+    button2() {
+      this.count2--
+      this.$socket.emit('count2', {count2: this.count2})
+    },
+    button3() {
+      this.count3--
+      this.$socket.emit('count3', {count3: this.count3})
+    }
   }
 }
 </script>
--- server/app.js
+++ server/app.js
@@ -11,6 +11,9 @@
   app.use(express.urlencoded({ extended: false }))
 
   let onlineUsers = []
+  let count1;
+  let count2;
+  let count3;
 
   io.on('connection', (socket) => {
       console.log('a user connected')
@@ -18,6 +21,21 @@
         console.log(username, 'ini username server')
         onlineUsers.push(username)
         io.emit('userLogin', onlineUsers)
+      } )
+      socket.on('count1', (objCount1) => {
+        console.log(objCount1, 'ini username count1')
+        count1 = objCount1.count1
+        io.emit('count1', count1)
+      } )
+      socket.on('count2', (objCount2) => {
+        console.log(objCount2, 'ini username count2')
+        count2 = objCount2.count2
+        io.emit('count2', count2)
+      } )
+      socket.on('count3', (objCount3) => {
+        console.log(objCount3, 'ini username count3')
+        count3 = objCount3.count3
+        io.emit('count3', count3)
       } )
   })
 
